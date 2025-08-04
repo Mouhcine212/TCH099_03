b31e88fb83f79e8d47b30354f2e6fd7db4725fc6
Mouhcine Imaoun <mouhcine.imaoun.1@ens.etsmtl.ca>
2025-08-04 08:09:08 -0400

    Update inscription.js

--- Site099/ETSbooking/inscription.js
+++ Site099/ETSbooking/inscription.js
@@ -61,7 +61,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     try {
-      const res = await fetch('https://flightets-gghremf5czh9d3ea.canadacentral-01.azurewebsites.net/api/endpoints/user_post.php', {
+      const res = await fetch( 'https://flightets-gghremf5czh9d3ea.canadacentral-01.azurewebsites.net/api/user', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
