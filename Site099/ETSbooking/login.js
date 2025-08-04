b1269263715a4f4d906a4e7dbefb1d4784d489e4
Mouhcine Imaoun <mouhcine.imaoun.1@ens.etsmtl.ca>
2025-08-04 05:55:27 -0400

    Update login.js

--- Site099/ETSbooking/login.js
+++ Site099/ETSbooking/login.js
@@ -6,7 +6,7 @@ document.getElementById('loginForm').addEventListener('submit', async function (
   const motDePasse = document.getElementById('motDePasse').value;
 
   try {
-    const response = await fetch('https://flightets-gghremf5czh9d3ea.canadacentral-01.azurewebsites.net/api/login', {
+    const response = await fetch('https://flightets-gghremf5czh9d3ea.canadacentral-01.azurewebsites.net/api/endpoints/login.php', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
