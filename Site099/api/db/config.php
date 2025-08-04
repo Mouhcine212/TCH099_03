c5a8266b8ab05c733d55c3e39bd962d03316b884
Mouhcine Imaoun <mouhcine.imaoun.1@ens.etsmtl.ca>
2025-08-04 05:57:56 -0400

    Update config.php

--- Site099/api/db/config.php
+++ Site099/api/db/config.php
@@ -4,7 +4,7 @@ interface Config
     const DB_HOST = "flightets-sql.mysql.database.azure.com"; // Serveur Azure
     const DB_USER = "adminflight@flightets-sql";              // Utilisateur Azure (avec suffixe)
     const DB_PWD  = "Mouhcine123";                            // Mot de passe Azure
-    const DB_NAME = "new_booker_updated";                     // Nom de la base
+    const DB_NAME = "flightets";                     // Nom de la base
 }
 
 define('DB_HOST', Config::DB_HOST);
